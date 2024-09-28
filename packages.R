06a3c92a4aa9b11bf23cc7f0e14b0454080b6c5c
Markus <Hello@Mackrics.com>
2024-09-28 17:47:47 +0200

    feat: add packages file

--- packages.R
+++ packages.R
@@ -1,1 +1,15 @@
 library(litedown)
+library(data.table)
+library(here)
+library(ggplot2)
+library(ricethemes)
+library(fs)
+library(patchwork)
+
+pak::lockfile_create(c(
+  "litedown",
+  "data.table",
+  "here",
+  "ggplot2",
+  "mackrics/ricethemes"
+))
