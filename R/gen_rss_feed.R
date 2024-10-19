25887f4d85b02f5149f4a056398e7919f3cae6bb
Markus <Hello@Mackrics.com>
2024-10-19 13:08:36 +0200

    feat: reduce dependencies for rss feed

--- R/gen_rss_feed.R
+++ R/gen_rss_feed.R
@@ -13,22 +13,18 @@ get_body <- function(file_path, url) {
   litedown::mark()
 }
 
-get_rss_items <- function(path_to_folder, base_url) {
-  path_data <-
-    data.frame(
-      path = list.files({{ path_to_folder }}, pattern = "*.Rmd", full.names = TRUE)
-    ) 
+get_rss_items <- function(path_to_folder, base_url, ignore_files = "blog/index.Rmd") {
+  documents <- list.files({{ path_to_folder }}, pattern = "*.Rmd", full.names = TRUE)
+  documents <-  documents[!documents %in% {{ ignore_files }}]
+  yaml <- lapply(documents, yaml_front_matter)
+  print(yaml)
   feed_data <-
-  data.frame(path = path_data[path_data$path != "blog/index.Rmd", ]) |>
-  within({
-    params = lapply(path, rmarkdown::yaml_front_matter)
-  }) |>
-  within({
-      date  = paste0("    <pubDate>", as.Date(as.character(lapply(params, \(x) x[[ "date" ]]))), "</pubDate>\n")
-      title = paste0("    <title>", lapply(params, \(x) x[[ "title" ]]), "</title>\n")
-      link  = paste0("    <guid>", base_url, "/", gsub("Rmd", "html", path), "</guid>\n")
-      desc  = paste0("    <description>", lapply(gsub("Rmd$", "html", path), \(x) get_body(x, {{ path_to_folder }})), "</description>\n")
-  }) 
+  data.frame(
+      date  = paste0("    <pubDate>", as.Date(as.character(lapply(yaml, \(x) x[[ "date" ]]))), "</pubDate>\n"),
+      title = paste0("    <title>", lapply(yaml, \(x) x[[ "title" ]]), "</title>\n"),
+      link  = paste0("    <guid>", base_url, "/", gsub("Rmd", "html", yaml), "</guid>\n"),
+      desc  = paste0("    <description>", lapply(gsub("Rmd$", "html", yaml), \(x) get_body(x, {{ path_to_folder }})), "</description>\n")
+  )
   o <- feed_data[order(feed_data$date), ]
   o <-
   paste0(paste0("  <item>\n", o$title, o$link, o$date, o$desc, "  </item>"), collapse = "\n")
