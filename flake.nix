e9465720423f6ec553abe0505e6a686f25e836e6
mackrics <hello@mackrics.com>
2025-04-13 21:24:37 +0200

    deps: use nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Build site";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.zola
+        ];
+      };
+    };
+}
+
+
